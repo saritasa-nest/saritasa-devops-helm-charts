ddd8c74bcf2bf69a43898a9da182e34fa55269f9
dmitry-mightydevops <dmitry@saritasa.com>
2023-02-19 16:05:54 -0600

    feat (tekton-apps): adjust release workflow

--- release.config.cjs
+++ release.config.cjs
@@ -41,7 +41,6 @@ module.exports = {
   branches: [
     '+([0-9])?(.{+([0-9]),x}).x',
     'main',
-    { name: 'beta', prerelease: true },
     { name: 'alpha', prerelease: true }
   ],
   plugins: [
